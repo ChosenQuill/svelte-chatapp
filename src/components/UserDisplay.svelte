6a94a24c9fea269ced1f73372c2f83fb9a68a7f3
SureDroid <support@suredroid.com>
2020-05-13 02:38:43 -0500

    Added couple more features to chatapp demo
    Typing Events, Join and Leave Events, Live User Display, Client and Server Side data validation, and User Highlighting.

---
+++ src/components/UserDisplay.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {typingUsers, users, loggedIn} from '../api.js'; 
+</script>
+
+{#if $loggedIn}
+    <div class="flex mt-5 shadow-lg px-4 py-2 justify-between">
+        <div>
+            {($typingUsers.length !== 0) ? $typingUsers.join(', ').concat(' is typing...') : ''}
+        </div>
+        <div>
+            {$users.join(', ')} | {$users.length} Online User{$users.length !== 1 ? 's' : ''}
+        </div>
+    </div>
+{/if}
